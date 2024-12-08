1a8b41e9f5029573e8d9603e54c61516b31c6a6d
“Omar <81875005+Trkzi-Omar@users.noreply.github.com>
2024-12-08 20:12:02 +0100

    Feat: layer-based basedOn options

--- src/components/MultipleLayersSetting/BasedOnDropdown.tsx
+++ src/components/MultipleLayersSetting/BasedOnDropdown.tsx
@@ -3,7 +3,7 @@ import styles from "./MultipleLayersSetting.module.css";
 import { useCatalogContext } from "../../context/CatalogContext";
 // Destructure props as an object
 interface BasedOnDropdownProps {
-  layerIndex?: number;
+  layerIndex: number;
 }
 export default function BasedOnDropdown({ layerIndex }: BasedOnDropdownProps) {
   const catalogContext = useCatalogContext();
@@ -20,6 +20,8 @@ export default function BasedOnDropdown({ layerIndex }: BasedOnDropdownProps) {
   const currentBasedon = layerIndex !== undefined 
     ? geoPoints[layerIndex]?.basedon || 'rating'
     : 'rating';
+
+  const layerNames = geoPoints[layerIndex]?.properties
 
   useEffect(function () {
     if (layerIndex !== undefined) {
@@ -54,11 +56,9 @@ export default function BasedOnDropdown({ layerIndex }: BasedOnDropdownProps) {
         onChange={handleSelectChange}
         className="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-md focus:ring-grey-100 focus:border-grey-100 block w-full p-1"
       >
-        <option value="rating">Rating</option>
-        <option value="user_ratings_total">Total Ratings</option>
-        <option value="priceLevel">Price Level</option>
-        <option value="business_status">Business Status</option>
-        <option value="heatmap_weight">Heatmap Weight</option>
+        {layerNames?.map((name: string, index: number) => (
+          <option key={index} value={name}>{name}</option>
+        ))}
       </select>
     </div>
   );
