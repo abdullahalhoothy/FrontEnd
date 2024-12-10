7097753ab4e1fdda889cf1d625046347709baf90
“Omar <81875005+Trkzi-Omar@users.noreply.github.com>
2024-12-10 23:58:44 +0100

    Undo: uncommit local config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/fastapi': {
-        target: 'http://s-locator.northernacs.com:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/fastapi/, '/fastapi'),
-      }
-    },
     port: 3000,
     host: "0.0.0.0",
   },
